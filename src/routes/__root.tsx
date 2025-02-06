c72b2234611615717de30cf8e654da81a15b74ca
David Dunham <ddunham@swishdata.com>
2025-02-06 14:49:51 -0500

    feat: expanded header with search box
    Signed-off-by: David Dunham <ddunham@swishdata.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,12 +9,29 @@ function Header() {
 
   return (
     <>
-      <div id="title" onClick={() => navigate({ to: "/" })} style={{}}>
-        <img
-          src="https://swishdata.zendesk.com/hc/theming_assets/01JBYJ39HYFD8G2P4GE9Z05PK1"
-          height={50}
-        />
-        <h1 style={{ color: "white" }}>Lasso Demo Help Desk</h1>
+      <div id="header">
+        <div id="header-contents">
+          <div id="title" onClick={() => navigate({ to: "/" })} style={{}}>
+            <img
+              src="https://swishdata.zendesk.com/hc/theming_assets/01JBYJ39HYFD8G2P4GE9Z05PK1"
+              height={50}
+            />
+          </div>
+          <h1>Welcome to Lasso Support</h1>
+          <p>
+            Search for answers to your questions by entering keywords below, or
+            look through our knowledge base.
+          </p>
+          <form id="knowledge-search">
+            <input
+              placeholder="Search the helpcenter..."
+              id="knowledge-input"
+            />
+            <button type="submit" id="knowledge-search-button">
+              Search
+            </button>
+          </form>
+        </div>
       </div>
       <div id="body">
         <Outlet />
