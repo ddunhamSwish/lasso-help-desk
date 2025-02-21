dd3b898fe8daf4e8a04d6499398edd7af8b78d0b
David Dunham <ddunham@swishdata.com>
2025-02-21 08:39:56 -0500

    fix: importing swish logo from local assets
    Signed-off-by: David Dunham <ddunham@swishdata.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet, useNavigate } from "@tanstack/react-router";
 import useQueryStore from "../store";
 import { useState } from "react";
+import swish_logo from "../assets/swish_logo_bw.png";
 
 export const Route = createRootRoute({
   component: Header,
@@ -21,10 +22,7 @@ function Header() {
       <div id="header">
         <div id="header-contents">
           <div id="title" onClick={() => navigate({ to: "/" })} style={{}}>
-            <img
-              src="https://swishdata.zendesk.com/hc/theming_assets/01JBYJ39HYFD8G2P4GE9Z05PK1"
-              height={50}
-            />
+            <img src={swish_logo} height={50} />
           </div>
           <h1>Welcome to Lasso Support</h1>
           <p>
